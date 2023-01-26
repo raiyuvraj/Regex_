3430d84c00a96bd45441c5c2bc67b68e4654deda
raiyuvraj <yuvirai017@gmail.com>
2023-01-26 18:08:30 +0530

    [Yuvraj]-Valid_First_Name

---
+++ Regex/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    static void Main(string[] args)
+     {
+            Console.WriteLine("Enter  First Name");
+            var data = Console.ReadLine();
+
+            string pattern = "^[A-Z]{1}[a-z]{2,}$";
+
+            Regex regex = new Regex(pattern);
+
+            if(regex.IsMatch(data))
+        {
+            Console.WriteLine("Is valid First Name");
+        }
+            else
+        {
+            Console.WriteLine("Invalid");
+        }
+    }
+}
+    
