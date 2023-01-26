d7db447ad16cf06f9be4e962ff9d66ae39cf33ce
raiyuvraj <yuvirai017@gmail.com>
2023-01-26 18:35:23 +0530

    [Yuvraj]-Valid_Password

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -8,15 +8,15 @@ public class Program
 {
     static void Main(string[] args)
      {
-            Console.WriteLine("Enter  Mobile No. :");
+            Console.WriteLine("Enter  Passowrd :");
             var data = Console.ReadLine();
 
-        string pattern = "^[+][][0-9]{1,3}[ ][6-9]{1}[0-9]{9}$";
+        string pattern = "^[A-z]{8}";
             Regex regex = new Regex(pattern);
 
             if(regex.IsMatch(data))
         {
-            Console.WriteLine("Is valid Mobile No.");
+            Console.WriteLine("Is valid Password");
         }
             else
         {
