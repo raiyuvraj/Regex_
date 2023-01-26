788666ef1995b24af8a924b205ed393b0fa92660
raiyuvraj <yuvirai017@gmail.com>
2023-01-26 18:30:52 +0530

    [Yuvraj]-Valid_Mobile_No.

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -8,15 +8,15 @@ public class Program
 {
     static void Main(string[] args)
      {
-            Console.WriteLine("Enter  Email Id. :");
+            Console.WriteLine("Enter  Mobile No. :");
             var data = Console.ReadLine();
 
-        string pattern = "^[0-9A-Za-z]+([.][0-9A-Za-z]+)[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        string pattern = "^[+][][0-9]{1,3}[ ][6-9]{1}[0-9]{9}$";
             Regex regex = new Regex(pattern);
 
             if(regex.IsMatch(data))
         {
-            Console.WriteLine("Is valid Email Id.");
+            Console.WriteLine("Is valid Mobile No.");
         }
             else
         {
